1af86d19c4ac914a1ac46ed5044b2d1a8ce34452
IvoTsochev <ivo.tsochev@live.com>
2022-03-12 10:49:52 +0200

    fixing the api call to the WP

--- src/components/SinglePortfolioItem/SinglePortfolioItem.jsx
+++ src/components/SinglePortfolioItem/SinglePortfolioItem.jsx
@@ -13,7 +13,7 @@ const SinglePortfolioItem = ({ singlePortfolio }) => {
   //   FEATURE IMAGE REQUEST
   axios
     .get(
-      `https://resources.ivaylotsochev.com/wp-json/wp/v2/media/${featured_media}`
+      `https://api.ivaylotsochev.com/wp-json/wp/v2/media/${featured_media}`
     )
     .then((res) => {
       setFeaturedImageUrl(res.data.media_details.sizes.medium.source_url);
@@ -21,13 +21,13 @@ const SinglePortfolioItem = ({ singlePortfolio }) => {
 
   return (
     <div className="singlePortfolioItem">
-      <a href={acf.project_url} target='_blank'>
-        <h4>{title.rendered}</h4>
-        <img className="singlePortfolioItem-img" src={featuredImageUrl} alt="" />
-        <div dangerouslySetInnerHTML={{ __html: excerpt.rendered }} />
-        <p>Project type: {acf.type}</p>
-        <p>Build on: {acf.build_on}</p>
-        <p>Used technologies: {acf.used_technologies}</p>
+      <a href={ acf.project_url } target='_blank'>
+        <h4>{ title.rendered }</h4>
+        <img className="singlePortfolioItem-img" src={ featuredImageUrl } alt="" />
+        <div dangerouslySetInnerHTML={ { __html: excerpt.rendered } } />
+        <p>Project type: { acf.type }</p>
+        <p>Build on: { acf.build_on }</p>
+        <p>Used technologies: { acf.used_technologies }</p>
       </a>
     </div>
   );
